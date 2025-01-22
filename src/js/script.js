fe7871d895509a0571e1c28490e275cff4e7df9f
daniloladeira <daniloladeira20@gmail.com>
2025-01-22 02:06:01 -0300

    Validações do preenchimento de formulário com JS.

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,31 @@ document.getElementById("toggle-senha").addEventListener("click", function() {
   toggleSenhaIcon.classList.toggle("fa-eye-slash");
   toggleSenhaIcon.classList.toggle("fa-eye");
 });
+
+document.querySelector("button").addEventListener("click", function() {
+  const nomeInput = document.getElementById("nome");
+  const cpfInput = document.getElementById("CPF");
+  const senhaInput = document.getElementById("senha");
+
+  if (nomeInput.value === "") {
+    alert("Campo nome é obrigatório!");
+    nomeInput.style.borderColor = "red";
+    return;
+  } else {
+    nomeInput.style.borderColor = "";
+  }
+
+  if (cpfInput.value === "") {
+    alert("Campo CPF é obrigatório!");
+    cpfInput.style.borderColor = "red";
+    return;
+  } else {
+    cpfInput.style.borderColor = "";
+  }
+
+  if(senhaInput.value === "") {
+    alert("Campo senha é obrigatório!");
+    senhaInput.style.borderColor = "red";
+    return;
+  }
+});
